ac9decc9ce1cb8d0eedbfab77bf91531856a4db6
Roma Yanush <vrong617@gmail.com>
2024-10-12 15:00:57 +0300

    Create task-2.js

---
+++ js/task-2.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const formatMessage = (message, maxLength) => message && maxLength && maxLength > 0 && message.length > maxLength ? message.slice(0, maxLength) + '...' : message; 
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
