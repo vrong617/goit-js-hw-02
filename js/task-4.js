58edcc83b5e2f1469bcca2a933b78249b82f301a
Roma Yanush <vrong617@gmail.com>
2024-10-12 15:02:12 +0300

    Create task-4.js

---
+++ js/task-4.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const getShippingCost = (country) => {
+  if (!country) { 
+    return;
+  }
+  
+  let price;
+
+  switch (country) {
+    case 'China':
+      price = 100;
+      break;
+    case 'Chile':
+      price = 250;
+      break;
+    case 'Australia':
+      price = 170;
+      break;
+    case 'Jamaica':
+      price = 120;
+      break;
+  }
+
+  return price > 0 ? `Shipping to ${country} will cost ${price} credits` : 'Sorry, there is no delivery to your country';
+}
+
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
